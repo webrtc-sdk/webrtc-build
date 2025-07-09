f18bbb5c3eca83a7df09d4116e18971300d2c3b3
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-07-09 14:47:18 +0200

    Revert: iOS only

--- build/apple/xcframework.sh
+++ build/apple/xcframework.sh
@@ -55,16 +55,16 @@ COMMON_ARGS="
 
 PLATFORMS=(
   "iOS-arm64-device:target_os=\"ios\" target_environment=\"device\" target_cpu=\"arm64\" ios_deployment_target=\"13.0\""
-  "iOS-arm64-simulator:target_os=\"ios\" target_environment=\"simulator\" target_cpu=\"arm64\" ios_deployment_target=\"13.0\""
-  "iOS-x64-simulator:target_os=\"ios\" target_environment=\"simulator\" target_cpu=\"x64\" ios_deployment_target=\"13.0\""
-  "macOS-arm64:target_os=\"mac\" target_cpu=\"arm64\" mac_deployment_target=\"10.15\""
-  "macOS-x64:target_os=\"mac\" target_cpu=\"x64\" mac_deployment_target=\"10.15\""
-  "catalyst-arm64:target_os=\"ios\" target_environment=\"catalyst\" target_cpu=\"arm64\" ios_deployment_target=\"14.0\""
-  "catalyst-x64:target_os=\"ios\" target_environment=\"catalyst\" target_cpu=\"x64\" ios_deployment_target=\"14.0\""
-  "tvOS-arm64-device:target_os=\"ios\" target_environment=\"appletv\" target_cpu=\"arm64\" ios_deployment_target=\"17.0\""
-  "tvOS-arm64-simulator:target_os=\"ios\" target_environment=\"appletvsimulator\" target_cpu=\"arm64\" ios_deployment_target=\"17.0\""
-  "xrOS-arm64-device:target_os=\"ios\" target_environment=\"xrdevice\" target_cpu=\"arm64\" ios_deployment_target=\"2.2.0\""
-  "xrOS-arm64-simulator:target_os=\"ios\" target_environment=\"xrsimulator\" target_cpu=\"arm64\" ios_deployment_target=\"2.2.0\""
+  # "iOS-arm64-simulator:target_os=\"ios\" target_environment=\"simulator\" target_cpu=\"arm64\" ios_deployment_target=\"13.0\""
+  # "iOS-x64-simulator:target_os=\"ios\" target_environment=\"simulator\" target_cpu=\"x64\" ios_deployment_target=\"13.0\""
+  # "macOS-arm64:target_os=\"mac\" target_cpu=\"arm64\" mac_deployment_target=\"10.15\""
+  # "macOS-x64:target_os=\"mac\" target_cpu=\"x64\" mac_deployment_target=\"10.15\""
+  # "catalyst-arm64:target_os=\"ios\" target_environment=\"catalyst\" target_cpu=\"arm64\" ios_deployment_target=\"14.0\""
+  # "catalyst-x64:target_os=\"ios\" target_environment=\"catalyst\" target_cpu=\"x64\" ios_deployment_target=\"14.0\""
+  # "tvOS-arm64-device:target_os=\"ios\" target_environment=\"appletv\" target_cpu=\"arm64\" ios_deployment_target=\"17.0\""
+  # "tvOS-arm64-simulator:target_os=\"ios\" target_environment=\"appletvsimulator\" target_cpu=\"arm64\" ios_deployment_target=\"17.0\""
+  # "xrOS-arm64-device:target_os=\"ios\" target_environment=\"xrdevice\" target_cpu=\"arm64\" ios_deployment_target=\"2.2.0\""
+  # "xrOS-arm64-simulator:target_os=\"ios\" target_environment=\"xrsimulator\" target_cpu=\"arm64\" ios_deployment_target=\"2.2.0\""
 )
 
 cd $SOURCE_DIR
@@ -96,21 +96,21 @@ start_group "Creating universal binaries (x64 + arm64)"
 
 rm -rf $OUT_DIR/*-lib $OUT_DIR/$FRAMEWORK_NAME.*
 
-mkdir -p $OUT_DIR/macOS-lib
-cp -R $OUT_DIR/macOS-x64/$FRAMEWORK_NAME.framework $OUT_DIR/macOS-lib/$FRAMEWORK_NAME.framework
-lipo -create -output $OUT_DIR/macOS-lib/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME $OUT_DIR/macOS-arm64/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME $OUT_DIR/macOS-x64/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
+# mkdir -p $OUT_DIR/macOS-lib
+# cp -R $OUT_DIR/macOS-x64/$FRAMEWORK_NAME.framework $OUT_DIR/macOS-lib/$FRAMEWORK_NAME.framework
+# lipo -create -output $OUT_DIR/macOS-lib/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME $OUT_DIR/macOS-arm64/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME $OUT_DIR/macOS-x64/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
 
-mkdir -p $OUT_DIR/catalyst-lib
-cp -R $OUT_DIR/catalyst-arm64/$FRAMEWORK_NAME.framework $OUT_DIR/catalyst-lib/$FRAMEWORK_NAME.framework
-lipo -create -output $OUT_DIR/catalyst-lib/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME $OUT_DIR/catalyst-arm64/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME $OUT_DIR/catalyst-x64/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
+# mkdir -p $OUT_DIR/catalyst-lib
+# cp -R $OUT_DIR/catalyst-arm64/$FRAMEWORK_NAME.framework $OUT_DIR/catalyst-lib/$FRAMEWORK_NAME.framework
+# lipo -create -output $OUT_DIR/catalyst-lib/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME $OUT_DIR/catalyst-arm64/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME $OUT_DIR/catalyst-x64/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
 
 mkdir -p $OUT_DIR/iOS-device-lib
 cp -R $OUT_DIR/iOS-arm64-device/$FRAMEWORK_NAME.framework $OUT_DIR/iOS-device-lib/$FRAMEWORK_NAME.framework
 lipo -create -output $OUT_DIR/iOS-device-lib/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME $OUT_DIR/iOS-arm64-device/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
 
-mkdir -p $OUT_DIR/iOS-simulator-lib
-cp -R $OUT_DIR/iOS-arm64-simulator/$FRAMEWORK_NAME.framework $OUT_DIR/iOS-simulator-lib/$FRAMEWORK_NAME.framework
-lipo -create -output $OUT_DIR/iOS-simulator-lib/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME $OUT_DIR/iOS-arm64-simulator/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME $OUT_DIR/iOS-x64-simulator/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
+# mkdir -p $OUT_DIR/iOS-simulator-lib
+# cp -R $OUT_DIR/iOS-arm64-simulator/$FRAMEWORK_NAME.framework $OUT_DIR/iOS-simulator-lib/$FRAMEWORK_NAME.framework
+# lipo -create -output $OUT_DIR/iOS-simulator-lib/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME $OUT_DIR/iOS-arm64-simulator/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME $OUT_DIR/iOS-x64-simulator/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
 
 end_group
 
@@ -118,13 +118,6 @@ start_group "Creating XCFramework"
 
 xcodebuild -create-xcframework \
   -framework $OUT_DIR/iOS-device-lib/$FRAMEWORK_NAME.framework \
-  -framework $OUT_DIR/iOS-simulator-lib/$FRAMEWORK_NAME.framework \
-  -framework $OUT_DIR/xrOS-arm64-device/$FRAMEWORK_NAME.framework \
-  -framework $OUT_DIR/xrOS-arm64-simulator/$FRAMEWORK_NAME.framework \
-  -framework $OUT_DIR/tvOS-arm64-device/$FRAMEWORK_NAME.framework \
-  -framework $OUT_DIR/tvOS-arm64-simulator/$FRAMEWORK_NAME.framework \
-  -framework $OUT_DIR/catalyst-lib/$FRAMEWORK_NAME.framework \
-  -framework $OUT_DIR/macOS-lib/$FRAMEWORK_NAME.framework \
   -output $OUT_DIR/$FRAMEWORK_NAME.xcframework
 
 end_group
@@ -133,13 +126,13 @@ start_group "Post-processing XCFramework"
 
 cp $SOURCE_DIR/LICENSE $OUT_DIR/$FRAMEWORK_NAME.xcframework/
 
-cd $OUT_DIR/$FRAMEWORK_NAME.xcframework/macos-arm64_x86_64/$FRAMEWORK_NAME.framework/
-mv $FRAMEWORK_NAME Versions/A/$FRAMEWORK_NAME
-ln -s Versions/Current/$FRAMEWORK_NAME $FRAMEWORK_NAME
+# cd $OUT_DIR/$FRAMEWORK_NAME.xcframework/macos-arm64_x86_64/$FRAMEWORK_NAME.framework/
+# mv $FRAMEWORK_NAME Versions/A/$FRAMEWORK_NAME
+# ln -s Versions/Current/$FRAMEWORK_NAME $FRAMEWORK_NAME
 
-cd $OUT_DIR/$FRAMEWORK_NAME.xcframework/ios-arm64_x86_64-maccatalyst/$FRAMEWORK_NAME.framework/
-mv $FRAMEWORK_NAME Versions/A/$FRAMEWORK_NAME
-ln -s Versions/Current/$FRAMEWORK_NAME $FRAMEWORK_NAME
+# cd $OUT_DIR/$FRAMEWORK_NAME.xcframework/ios-arm64_x86_64-maccatalyst/$FRAMEWORK_NAME.framework/
+# mv $FRAMEWORK_NAME Versions/A/$FRAMEWORK_NAME
+# ln -s Versions/Current/$FRAMEWORK_NAME $FRAMEWORK_NAME
 
 cd $OUT_DIR
 zip --symlinks -9 -r $FRAMEWORK_NAME.xcframework.zip $FRAMEWORK_NAME.xcframework
